7105ab7ee636e0b1972d8ec53e22ebb3dab1b267
AlexEnCode <alexgabrielline@gmail.com>
2024-01-29 16:41:55 +0100

    Ajoutez des fichiers projet.

---
+++ WebApplicationDemo1/Controllers/ContactController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationDemo1.Controllers
+{
+    public class ContactController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Details()
+        {
+            return View();
+        }
+        public ActionResult Add()
+        {
+            return View();
+        }
+    }
+}
