2bc23888ba391d1063a6f2f210e36ff6193e900f
AlexEnCode <alexgabrielline@gmail.com>
2024-01-30 15:48:42 +0100

    features : contact generate

--- WebApplicationDemo1/Controllers/ContactController.cs
+++ WebApplicationDemo1/Controllers/ContactController.cs
@@ -1,32 +1,60 @@
-﻿using System.Threading.Channels;
+﻿using System.Reflection;
+using System.Threading.Channels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using WebApplicationDemo1.Models;
 
 namespace WebApplicationDemo1.Controllers
 {
-    public class ContactController : Controller
-    {
+	public class ContactController : Controller
+	{
+		List<Contact> listeContacts = new List<Contact>()
+			{
+			new Contact(1, "Jean", "Pleutre", "jp@oui.com"),
+			new Contact(2, "Marie", "Curie", "marie@gmail.com"),
+			new Contact(3, "Paul", "Dupont", "paul@yahoo.com"),
+			new Contact(4, "Alice", "Martin", "alice@yahoo.com"),
+			new Contact(5, "Bob", "Johnson", "bob@gmail.com"),
+			new Contact(6, "Claire", "Dupuis", "claire@hotmail.com"),
+			new Contact(7, "David", "Lefevre", "david@gmail.com"),
+			new Contact(8, "Emma", "Leclerc", "emma@yahoo.com"),
+			new Contact(9, "Frank", "Bertrand", "frank@hotmail.com"),
+			new Contact(10, "Grace", "Lamoureux", "grace@gmail.com"),
+			new Contact(11, "Hugo", "Rousseau", "hugo@yahoo.com"),
+			new Contact(12, "Isabelle", "Moreau", "isabelle@hotmail.com"),
+			new Contact(13, "Jack", "Gagnon", "jack@gmail.com")
+			};
 
-        public ActionResult Index()
-        {
-            return View();
-        }
-        public ActionResult Details()
-        {
-            NewContact nouveauContact = new("Jean", "Pleutre", "jp@oui.com");
 
-            ViewData["Nouveau Contact"] = nouveauContact;
-            ViewBag.nouveauContact = nouveauContact;
+		public ActionResult Index()
+		{
+
+			return View(listeContacts);
+		}
 
 
-            return View(nouveauContact);
-        }
-        public ActionResult Add()
-        {
-            return View();
-        }
+		public ActionResult Details()
+		{
 
-    }
+
+
+			return View();
+		}
+
+		public ActionResult Add()
+		{
+			return View();
+		}
+
+		public ActionResult FindbyID(int rechercheid)
+		{
+
+			Contact contact = listeContacts.FirstOrDefault(c => c.Id == rechercheid) ??  new Contact(404, "Je n'existe", "Pas", "déso@oui.com");
+
+			if (contact != null)
+				return View(contact); //contact.Nom contact.Prenom contact.Email
+			else return View();
+
+		}
+	}
 }
-
--- WebApplicationDemo1/Models/Contact.cs
+++ WebApplicationDemo1/Models/Contact.cs
@@ -1,17 +1,20 @@
 ﻿namespace WebApplicationDemo1.Models
 {
-    public class NewContact
+    public class Contact
     {
+        public int id; 
         private string nom = "Gontrand";
         private string prenom = "Flibustier";
         private string email = "nomail@flambi.fr";
 
-        public string Nom { get => nom; set => nom = value; }
+        public int Id { get => id; set => id = value; }
+		public string Nom { get => nom; set => nom = value; }
         public string Prenom { get => prenom; set => prenom = value; } 
         public string Email { get => email; set => email = value; }
 
-        public NewContact(string nom, string prenom, string email)
+        public Contact(int id, string nom, string prenom, string email)
         {
+            Id = id;
             Nom = nom;
             Prenom = prenom;
             Email = email;
