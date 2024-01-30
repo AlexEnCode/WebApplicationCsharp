9a2c5023d7e2feee7a110fc92789e55d333d6a07
AlexEnCode <alexgabrielline@gmail.com>
2024-01-30 11:02:57 +0100

    feat : model creation

--- WebApplicationDemo1/Controllers/ContactController.cs
+++ WebApplicationDemo1/Controllers/ContactController.cs
@@ -1,21 +1,32 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Threading.Channels;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using WebApplicationDemo1.Models;
 
 namespace WebApplicationDemo1.Controllers
 {
     public class ContactController : Controller
     {
+
         public ActionResult Index()
         {
             return View();
         }
         public ActionResult Details()
         {
-            return View();
+            NewContact nouveauContact = new("Jean", "Pleutre", "jp@oui.com");
+
+            ViewData["Nouveau Contact"] = nouveauContact;
+            ViewBag.nouveauContact = nouveauContact;
+
+
+            return View(nouveauContact);
         }
         public ActionResult Add()
         {
             return View();
         }
+
     }
 }
+
---
+++ WebApplicationDemo1/Models/NewContact.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApplicationDemo1.Models
+{
+    public class NewContact
+    {
+        private string nom = "Gontrand";
+        private string prenom = "Flibustier";
+        private string email = "nomail@flambi.fr";
+
+        public string Nom { get => nom; set => nom = value; }
+        public string Prenom { get => prenom; set => prenom = value; } 
+        public string Email { get => email; set => email = value; }
+
+        public NewContact(string nom, string prenom, string email)
+        {
+            Nom = nom;
+            Prenom = prenom;
+            Email = email;
+        }
+    }
+}
